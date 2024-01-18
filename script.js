705eaa9e36f88091993052bbf92b5572cdf06318
Sairam-50 <sairams.shinrai74@gmail.com>
2024-01-18 19:16:45 +0530

    done

--- script.js
+++ script.js
@@ -1,22 +1,70 @@
 const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
-  // You will be using this as an event listener for the sortButton. Because buttons associated with a form element submit by default, you need to prevent that behavior.
   event.preventDefault();
-  // you need to get the values from your select elements. Since they all have the class values-dropdown, you can query them all at once.
 
   const inputValues = [
-    ...document.getElementsByClassName("values-dropdown"),
+    ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
-  updateUI(inputValues);
-};
 
-// this function is used for updating the UI.
-// Because you will be writing algorithms that won't immediately have a return value, set a fallback value for array to be an empty array.
-const updateUI = (array=[]) => {
-    array.forEach((num, i) => {
-        const outputValueNode = document.getElementById(`output-value-${i}`);
-        outputValueNode.innerText = num;
-    });
-};
+  const sortedValues = inputValues.sort((a, b) => {
+    return a - b;
+  });
+
+  updateUI(sortedValues);
+}
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  })
+}
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+}
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+
+  return array;
+}
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
+  }
+  return array;
+}
+
 sortButton.addEventListener("click", sortInputArray);
